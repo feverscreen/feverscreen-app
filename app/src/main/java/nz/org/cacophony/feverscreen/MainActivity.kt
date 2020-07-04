b5eea269e2cc82a3409b7a23c83c1f31be378c93
cam <cameron.ryan.pears@gmail.com>
2020-07-05 00:48:22 +1200

    Will automatically open up the deivce page if only one deivce is found.

--- app/src/main/java/nz/org/cacophony/feverscreen/MainActivity.kt
+++ app/src/main/java/nz/org/cacophony/feverscreen/MainActivity.kt
@@ -4,7 +4,11 @@ import android.content.Context
 import android.net.nsd.NsdManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
+import kotlin.concurrent.thread
 
 const val TAG = "feverscreen"
 
@@ -12,13 +16,14 @@ class MainActivity : AppCompatActivity() {
 
     private val deviceListAdapter = MutableLiveData<DeviceListAdapter>()
     private lateinit var deviceManager: DeviceManager
+    private lateinit var deviceList: DeviceList
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val nsdManager = applicationContext.getSystemService(Context.NSD_SERVICE) as NsdManager
-        val deviceList = DeviceList()
+        deviceList = DeviceList()
         deviceList.setOnChanged { onDeviceUpdate() }
         deviceListAdapter.value = DeviceListAdapter(deviceList)
         deviceManager = DeviceManager(nsdManager, deviceList, this)
@@ -27,6 +32,34 @@ class MainActivity : AppCompatActivity() {
             setHasFixedSize(true)
             layoutManager = recyclerLayoutManager
             adapter = deviceListAdapter.value
+        }
+        scanningView(true)
+        autoOpen()
+    }
+
+    private fun scanningView(scanning: Boolean) {
+        if (scanning) {
+            findViewById<LinearLayout>(R.id.device_scanning_layout).visibility = View.VISIBLE
+            findViewById<androidx.recyclerview.widget.RecyclerView>(R.id.device_recycle_view).visibility = View.GONE
+        } else {
+            findViewById<LinearLayout>(R.id.device_scanning_layout).visibility = View.GONE
+            findViewById<androidx.recyclerview.widget.RecyclerView>(R.id.device_recycle_view).visibility = View.VISIBLE
+        }
+    }
+
+    private fun autoOpen() {
+        thread {
+            Thread.sleep(5000)
+            runOnUiThread {
+                scanningView(false)
+                when (deviceList.size()) {
+                    0 -> Toast.makeText(applicationContext,"No cameras found", Toast.LENGTH_SHORT).show()
+                    1 -> deviceList.elementAt(0).openManagementInterface()
+                    else -> {
+                        Toast.makeText(applicationContext,"Multiple cameras found. Select what one to view", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
         }
     }
 
