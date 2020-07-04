f1e373161ac0182f7b245548bab39987dfa1e877
cam <cameron.ryan.pears@gmail.com>
2020-07-04 23:48:41 +1200

    Hide system UI buttons in feverscreen view

--- app/src/main/java/nz/org/cacophony/feverscreen/FeverWebView.kt
+++ app/src/main/java/nz/org/cacophony/feverscreen/FeverWebView.kt
@@ -2,11 +2,16 @@ package nz.org.cacophony.feverscreen
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 
 class FeverWebView : AppCompatActivity() {
+
+    private var mDetector: GestureDetector? = null
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,5 +27,28 @@ class FeverWebView : AppCompatActivity() {
             myWebView.webViewClient = WebViewClient()
             myWebView.loadUrl("$uri/static/html/fever.html")
         }
+
+        hideSystemUI()
+
+        val myView = findViewById<View>(R.id.fever_web_view)
+        mDetector = GestureDetector(this, HideSystemUiGestureListener {hideSystemUI()})
+        myView.setOnTouchListener { _, event -> mDetector!!.onTouchEvent(event) }
+    }
+
+    class HideSystemUiGestureListener(val hideSystemUi: (() -> Unit)) : GestureDetector.SimpleOnGestureListener() {
+        override fun onSingleTapConfirmed(e: MotionEvent): Boolean {
+            hideSystemUi()
+            return true
+        }
+    }
+
+    private fun hideSystemUI() {
+        window.decorView.systemUiVisibility = (
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)
     }
 }
