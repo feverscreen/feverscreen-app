d2e6e20a827b8c06f2f6d284fcf98c20409b842e
cam <cameron.ryan.pears@gmail.com>
2020-08-03 21:57:10 +1200

    Update version to 0.1.2

--- app/src/main/java/nz/org/cacophony/feverscreen/MainActivity.kt
+++ app/src/main/java/nz/org/cacophony/feverscreen/MainActivity.kt
@@ -10,6 +10,7 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.MutableLiveData
@@ -28,6 +29,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<TextView>(R.id.version_text).text = "Feverscreen version: " + BuildConfig.VERSION_NAME
 
         val nsdManager = applicationContext.getSystemService(Context.NSD_SERVICE) as NsdManager
         deviceList = DeviceList()
