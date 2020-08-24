b6e03f53cfca4cddd0f98fae0c93f5c48a8b668d
cam <cameron.ryan.pears@gmail.com>
2020-08-25 10:50:58 +1200

    Close power warning when plugged back into power

--- app/src/main/java/nz/org/cacophony/feverscreen/AlertDialogActivity.kt
+++ app/src/main/java/nz/org/cacophony/feverscreen/AlertDialogActivity.kt
@@ -2,9 +2,17 @@ package nz.org.cacophony.feverscreen
 
 import android.app.Activity
 import android.app.AlertDialog
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
 import android.os.Bundle
 
 class AlertDialogActivity : Activity() {
+
+    private var receiver: BroadcastReceiver? = null
+    private var alertDialog: AlertDialog? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,6 +21,33 @@ class AlertDialogActivity : Activity() {
         builder.setTitle("Device is not charging")
         builder.setMessage("Please attach the tablet to a charger")
         builder.setOnDismissListener { super.onBackPressed() }
-        builder.show()
+        alertDialog = builder.create()
+        alertDialog?.show()
+
+        val filter = IntentFilter()
+        filter.addAction("android.intent.action.ACTION_POWER_CONNECTED")
+        filter.addAction("android.intent.action.ACTION_POWER_DISCONNECTED")
+
+        receiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context, i: Intent) {
+                Thread.sleep(1000) // wait for USB power state to be updated
+                val intent = context.registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+                val plugged = intent?.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1)
+                val charging = plugged == BatteryManager.BATTERY_PLUGGED_AC || plugged == BatteryManager.BATTERY_PLUGGED_USB
+                if (charging) {
+                    this@AlertDialogActivity.onDestroy()
+                }
+            }
+        }
+        registerReceiver(receiver, filter)
+    }
+
+    override fun onDestroy() {
+        alertDialog?.cancel()
+        if (receiver != null) {
+            unregisterReceiver(receiver)
+            receiver = null
+        }
+        super.onDestroy()
     }
 }
